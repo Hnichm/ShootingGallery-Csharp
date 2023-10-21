fa05cb620116022563baf33161e12cf3516b1d89
Hnichm <125336099+Hnichm@users.noreply.github.com>
2023-10-21 11:45:29 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShootingGallery
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D targetSprite;
+        Texture2D crosshairsSprite;
+        Texture2D backgroundSprite;
+        SpriteFont gameFont;
+
+        Vector2 targetPosition = new Vector2(300, 300);
+        const int targetRadius = 45;
+
+        MouseState mState;
+        bool mReleased = true;
+        int score = 0;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            targetSprite = Content.Load<Texture2D>("target");
+            crosshairsSprite = Content.Load<Texture2D>("crosshairs");
+            backgroundSprite = Content.Load<Texture2D>("sky");
+            gameFont = Content.Load<SpriteFont>("galleryFont");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            mState = Mouse.GetState();
+
+            if (mState.LeftButton == ButtonState.Pressed && mReleased == true)
+            {
+                mReleased = false;
+                score++;
+            }
+            if (mState.LeftButton == ButtonState.Released)
+            {
+                mReleased = true;
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.DeepPink);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
+            _spriteBatch.DrawString(gameFont, score.ToString(), new Vector2(100, 100), Color.White);
+            _spriteBatch.Draw(targetSprite, targetPosition, Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
